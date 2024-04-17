cd522cf17c166b93cdd07509130136fc18f8a505
Glorinha <64878674+LiviaBraz@users.noreply.github.com>
2024-04-16 21:19:45 -0300

    :sparkles : feat: exercicios em aula

---
+++ Programação Estruturada/Aula 08/aula08.py
@@ -0,0 +1,11 @@
+#nome = input("Digite seu primeiro nome:\n")
+#sobrenome = input("Digite seu sobrenome:\n")
+
+#print("Seu nome completo é",nome , sobrenome)
+
+#------------------
+
+nome = input("Digite seu nome:\n")
+qntd = int(input("Quantas vezes deseja ver? "))
+
+print(("\n" + nome) * qntd) 
